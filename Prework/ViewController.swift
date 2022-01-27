f504b819bb7e5c5cf9ce2e827f3efde18ddda1f1
khalilSS <61631655+khalilSS@users.noreply.github.com>
2022-01-27 13:30:32 -0500

    Added Prework Files to Repository

---
+++ Prework/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Prework
+//
+//  Created by Khalil Scott-Shepherd on 1/25/22.
+//  Copyright © 2022 Khalil Scott-Shepherd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func calculateTip(_ sender: Any) {
+        let bill = Double(billAmountTextField.text!) ?? 0
+        let tipPercentages = [0.15,0.18,0.2]
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
